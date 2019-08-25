4c02e30e86181e2a8345ffd0e6b21cba5939d617
Saran Ahuja <saran.sunny@gmail.com>
2019-08-25 15:33:31 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import sys
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+# get channel_secret and channel_access_token from your environment variable
+CHANNEL_SECRET  = os.getenv('CHANNEL_SECRET', None)
+CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN', None)
+if CHANNEL_SECRET is None:
+    print('Specify CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if CHANNEL_ACCESS_TOKEN is None:
+    print('Specify CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+print(CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN)
+print(type(CHANNEL_ACCESS_TOKEN))
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN, timeout=20)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+@app.route('/callback', methods=['GET', 'POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    #handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    #test push message
+    #line_bot_api.push_message(USER_ID,TextSendMessage(text='Hello World!') )
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', default=5000, help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, port=options.port)
